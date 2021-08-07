41cb9c097ccb20f8eb6ba875a5c435ae78b2d157
tagty <bouzu.ao@gmail.com>
2021-08-07 16:55:43 +0900

    add: mysql host

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ echo 'Started to deploy.'
 
 # ssh isucon10-qualify1 "cd /home/isucon/isuumo && git pull && cd /home/isucon/isuumo/webapp/go && make isuumo && sudo systemctl restart mysql && sudo systemctl restart isuumo.go && sudo systemctl restart nginx && sudo sysctl -p"
 
-ssh isucon@18.181.180.238 "cd /home/isucon/isuumo && git pull && cd /home/isucon/isuumo/webapp/go && make isuumo && sudo systemctl restart mysql && sudo systemctl restart isuumo.go && sudo systemctl restart nginx && sudo sysctl -p"
+ssh isucon@18.182.2.100 "cd /home/isucon/isuumo && git pull && cd /home/isucon/isuumo/webapp/go && make isuumo && sudo systemctl restart mysql && sudo systemctl restart isuumo.go && sudo systemctl restart nginx && sudo sysctl -p"
 
 echo 'Finished to deploy.'
